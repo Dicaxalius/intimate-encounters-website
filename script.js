5329ce2c071a68d420ef5c30cc7542ca634ad3a8
Dicaxalius <66039221+Dicaxalius@users.noreply.github.com>
2024-02-26 09:45:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -90,4 +90,23 @@ particlesJS('particles-js', {
   },
   retina_detect: true // Adjusts particles for retina displays
 });
+let slideIndex = 0;
+showSlides();
+
+function moveSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("testimonial-slide");
+    if (slideIndex >= slides.length) { slideIndex = 0 }
+    if (slideIndex < 0) { slideIndex = slides.length - 1 }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
 
