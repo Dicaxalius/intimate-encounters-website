e814cca7dd846b10956fac337efef1b41f25cc7d
Dicaxalius <66039221+Dicaxalius@users.noreply.github.com>
2024-02-25 20:25:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// This script will be expanded to add dynamic functionality to the site.
+document.addEventListener('DOMContentLoaded', function () {
+    const inputs = document.querySelectorAll('.form-control');
+
+    inputs.forEach(input => {
+        input.addEventListener('input', function () {
+            const formGroup = this.parentElement;
+            if (this.checkValidity()) {
+                formGroup.classList.remove('invalid');
+                formGroup.classList.add('valid');
+            } else {
+                formGroup.classList.add('invalid');
+                formGroup.classList.remove('valid');
+            }
+        });
+    });
+});
+
